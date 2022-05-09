4b2e142fd89692fc12f1db597f64971574b40af7
georgianacrisan99@gmail.com <georgianacrisan99@gmail.com>
2022-05-09 14:20:07 +0300

    End-Points made

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const messagesRouter = require('./router/messagesRouter');
+const connection = require("./database.js");
+
+
+
 const app = express();
-app.use(cors())
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/diary",messagesRouter);
+
+
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
 app.listen(port, () => {
   console.log(`Cloud comp app listening on port ${port}!`)
 });
+
+
---
+++ router/messagesRouter.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");
+const connection = require("../database.js");
+
+//Get the messages
+router.get("/", (req, res) => {
+    connection.query("SELECT ID, writerName, writerMail, DATE_FORMAT(date_write,'%e-%M-%Y') as date, subject, content FROM diary", (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            message: results,
+        })
+    })
+
+});
+
+//Insert a message
+router.post("/", (req, res) => {
+    console.log(req.body);
+
+    const {
+        writerName,
+        writerMail,
+        date_write,
+        subject,
+        content
+    } = req.body;
+    if (!writerName || !writerMail || !date_write || !subject || !content) {
+        return res.status(400).json({
+            error: "All fields are required!",
+        })
+    }
+
+    connection.query(`INSERT into diary (writerName, writerMail, date_write, subject, content) values (${mysql.escape(writerName)}, ${mysql.escape(writerMail)}, ${mysql.escape(date_write)}, ${mysql.escape(subject)},${mysql.escape(content)}) `, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            message: results,
+        })
+    })
+
+});
+
+
+//Get the messages by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        // send bad request error
+        return res.status(400).send("Bad request. Missing parameters.");
+    }
+
+    connection.query(`SELECT ID, writerName, writerMail, DATE_FORMAT(date_write,'%e-%M-%Y') as date, subject, content FROM diary WHERE ID= ${mysql.escape(id)}`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            message: results,
+        })
+    })
+
+});
+
+//Delete by id
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        // send bad request error
+        return res.status(400).send("Bad request. Missing parametres.");
+    }
+    const queryString = `DELETE FROM diary WHERE ID = ${mysql.escape(id)}`;
+    connection.query(queryString, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        if (results.length === 0) {
+            return res.status(404).send("Message not found.");
+        }
+        return res.json({
+            results,
+        });
+    }
+    );
+}
+);
+
+//Update by id
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        // send bad request error
+        return res.status(400).send("Bad request. Missing parametres.");
+    }
+    const {
+        writerName,
+        writerMail,
+        date_write,
+        subject,
+        content
+    } = req.body;
+    if (!writerName || !writerMail || !date_write || !subject || !content) {
+        return res.status(400).json({
+            error: "All fields are required!",
+        })
+    }
+
+
+    const queryString = `UPDATE diary SET writerName = ${mysql.escape(writerName)}, writerMail = ${mysql.escape(writerMail)}, date_write = ${mysql.escape(date_write)}, subject = ${mysql.escape(subject)}, content = ${mysql.escape(content)} WHERE ID = ${mysql.escape(id)}`;
+    connection.query(queryString, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        if (results.length === 0) {
+            return res.status(404).send("Message not found.");
+        }
+        return res.json({
+            results,
+        });
+    }
+    );
+}
+);
+
+
+module.exports = router;
